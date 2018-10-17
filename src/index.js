92248d1bed87790fe21d26a99b055b309b668409
aliceljne <alice.lejeune@u-psud.fr>
2018-10-17 22:47:42 +0200

    bug json

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import _ from "underscore"
 import $ from "jquery"
-import plotly from "plotly.js/dist/plotly"
+import Plotly from "plotly.js/dist/plotly"
 
 $(document).ready(function() {
 	console.log("ok")
 
 	var TESTER = document.getElementById('tester');
-	plotly.plot( TESTER, [{
-	x: [1, 2, 3, 4, 5],
-	y: [1, 2, 4, 8, 16] }], {
+
+	Plotly.plot( TESTER, [{
+    x: [1, 2, 3, 4, 5],
+    y: [1, 2, 4, 8, 16] }], { 
 	margin: { t: 0 } } );
+	
+	var year1 = require("../Data/2015_16.json")
+	var year2 = require("../Data/2016_17.json")
+	console.log(year1)
+	console.log(year2)
 });
