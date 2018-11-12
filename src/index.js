24c1e1419aa285b118045418a62f928776fe2b25
aliceljne <alicelejeune96@gmail.com>
2018-11-12 21:59:47 +0100

    add a chart example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from "underscore"
 import $ from "jquery"
 import Plotly from "plotly.js/dist/plotly"
+import Chart from 'chart.js';
 
 $(document).ready(function() {
 	console.log("ok")
@@ -16,4 +17,43 @@ $(document).ready(function() {
 	var year2 = require("../Data/2016_17.json")
 	console.log(year1)
 	console.log(year2)
+
+	var ctx = document.getElementById("myChart");
+	
+	var myChart = new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
 });
