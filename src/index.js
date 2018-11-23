0b054f34881ed49fc93d7afbab38b0fc1a8543f8
maxtkl <maxime287@hotmail.com>
2018-11-23 14:06:51 +0100

    data ok fot the chart

--- src/index.js
+++ src/index.js
@@ -2,36 +2,81 @@ import $ from "jquery"
 import Plotly from 'plotly.js-dist';
 import Chart from "chart.js";
 
-$(document).ready(function() {
-	console.log("ok ok ok")
+// récupère un objet et une journée et retourne un tableau avec les equipes
+// avec leur nombre de points 
+function point(journee_max, year_obj) {
+	var temp = {}
+	var cpt = 0
+	//initialisation de l'objet avec les equipes en clé et 0 en nombre de points
+	for (var i = 0; i < year_obj.journee_01.length; i++) {
+		for (var j = 0; j < year_obj.journee_01[i].match.length; j++) {
+			temp[year_obj.journee_01[i].match[j].domicile] = 0
+			temp[year_obj.journee_01[i].match[j].exterieur] = 0
+		}
+	}
 
+	// on boucle sur toute les journées d'une année
+	for (var journee in year_obj) {
+		cpt ++
+		// on boucle sur toutes les dates
+		for (var i = 0; i < year_obj[journee].length; i++) {
+			// on boucle sur les matchs de chaque date
+			for (var j = 0; j < year_obj[journee][i].match.length; j++) {
+				if (year_obj[journee][i].match[j].but_dom > year_obj[journee][i].match[j].but_ext) {
+					temp[year_obj[journee][i].match[j].domicile] += 3
+				} else if (year_obj[journee][i].match[j].but_dom < year_obj[journee][i].match[j].but_ext) {
+					temp[year_obj[journee][i].match[j].exterieur] += 3
+				} else {
+					temp[year_obj[journee][i].match[j].domicile] ++
+					temp[year_obj[journee][i].match[j].exterieur] ++
+				}
+			}
+		}
+		// on sort de la boucle quand on a atteind le nombre de journée rentré
+		if (cpt == journee_max) {
+			break;
+		}
+	}
+	var result = [[],[]]
+	var i = 0
+	for (var team in temp) {
+		result[0][i] = team
+		result[1][i] = temp[team]
+		i++
+	}
+	i = 0
+	return result
+}
+
+$(document).ready(function() {
 	var year1 = require("../Data/2015_16.json")
 	var year2 = require("../Data/2016_17.json")
 	var year3 = require("../Data/2017_18.json")
 
+	// console.log(year2.journee_01[0].match[0].but_dom)
+	// tableau des equipes de la première année
 	var equipes = []
-	
-	for (var i = 0; i < year1.journee_1.length; i++) {
-		for (var j = 0; j < year1.journee_1[i].match.length; j++) {
-			equipes.push(year1.journee_1[i].match[j].domicile)
-			equipes.push(year1.journee_1[i].match[j].exterieur)
+	for (var i = 0; i < year2.journee_01.length; i++) {
+		for (var j = 0; j < year2.journee_01[i].match.length; j++) {
+			equipes.push(year2.journee_01[i].match[j].domicile)
+			equipes.push(year2.journee_01[i].match[j].exterieur)
 		}
 	}
+	equipes.sort()
 
-	console.log(year1.journee_1)
+	console.log(year2)
 	console.log(equipes)
-
-
+	var point_final = point(38,year2)
+	console.log(point(3,year2))
 
 	var ctx = document.getElementById("myChart");
-	
 	var myChart = new Chart(ctx, {
 		type: 'bar',
 		data: {
-			labels: equipes,
+			labels: point_final[0],
 			datasets: [{
-				label: '# of Votes',
-				data: [12, 19, 3, 5, 2, 3],
+				label: 'points',
+				data: point_final[1],
 				backgroundColor: [
 					'rgba(255, 99, 132, 0.2)',
 					'rgba(54, 162, 235, 0.2)',
